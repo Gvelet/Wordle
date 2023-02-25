6321988f5fd9e90998a5ffd3cc1bb19e031a212e
Michail Lemeshev <MilemX11@gmail.com>
2023-02-26 00:49:11 +0500

    adding a playing field to the page

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@ import {wordsArray} from './arrayWords.js'
 
 let randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
+const NUMBER_ATTEMPTS = 6;
+
+function renderingField(){
+  const gameBoard = document.querySelector('.game__board');
+
+  for(let i = 0; i<NUMBER_ATTEMPTS; i++){
+    const rowsBoard = document.createElement('div');
+    rowsBoard.classList = 'game__row';
+
+    gameBoard.append(rowsBoard);
+
+    for(let j = 0; j<5; j++){
+      const cellsBoard = document.createElement('div');
+      cellsBoard.classList = 'game__cell';
+
+      rowsBoard.append(cellsBoard);
+    }
+  }
+} 
+
+renderingField()
